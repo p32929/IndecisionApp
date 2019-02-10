69e8d626eac05640de7c32ba85a659b9ef85ae87
p32929 <p32929@yahoo.com>
2019-02-10 19:41:52 +0600

    .

--- src/Playground/IndecisionApp/AppComponents/Header.js
+++ src/Playground/IndecisionApp/AppComponents/Header.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 const Header = (props) => (
-    <div>
+    <div
+        className='header'
+    >
         <h1>{props.title}</h1>
         <h2>{props.subtitle}</h2>
     </div>
