806089a55ec440174912ddeb8ecb381b067fb8b3
p32929 <p32929@yahoo.com>
2019-02-10 20:30:07 +0600

    .

--- src/Playground/IndecisionApp/AppComponents/Header.js
+++ src/Playground/IndecisionApp/AppComponents/Header.js
@@ -4,8 +4,8 @@ const Header = (props) => (
     <div
         className='header'
     >
-        <h1>{props.title}</h1>
-        <h2>{props.subtitle}</h2>
+        <h1 className='header_title'>{props.title}</h1>
+        <h2 className='header_subtitle'>{props.subtitle}</h2>
     </div>
 )
 
