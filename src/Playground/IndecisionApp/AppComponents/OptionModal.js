f317fef2d956e41df542f3b6fde1635dcf9f396c
p32929 <p32929@yahoo.com>
2019-02-11 19:44:24 +0600

    modal css done

--- src/Playground/IndecisionApp/AppComponents/OptionModal.js
+++ src/Playground/IndecisionApp/AppComponents/OptionModal.js
@@ -7,13 +7,15 @@ const OptionModal = (props) => {
             isOpen={!!props.selectedOption}
             contentLabel="Selected Option"
             onRequestClose={props.handleClearSelectedOption}
+            closeTimeoutMS={200}
+            className='modal'
         >
-            <h3>Selected Option</h3>
+            <h3 className='modal__title'>Selected Option</h3>
             {
-                props.selectedOption && <p>{props.selectedOption}</p>
+                props.selectedOption && <p className='modal__body'>{props.selectedOption}</p>
             }
 
-            <button
+            <button className='add_options_button'
                 onClick={props.handleClearSelectedOption}
             >OK</button>
 
