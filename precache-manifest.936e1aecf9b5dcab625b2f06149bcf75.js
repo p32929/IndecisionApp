4f1492209f30717ba3ab1d402f49c93204297265
p32929 <p32929@yahoo.com>
2019-02-11 20:31:23 +0600

    Updates

---
+++ precache-manifest.936e1aecf9b5dcab625b2f06149bcf75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c7aabf0b03a539205c1f",
+    "url": "/IndecisionApp/static/js/runtime~main.c7aabf0b.js"
+  },
+  {
+    "revision": "34ae31d2d5f97a853eed",
+    "url": "/IndecisionApp/static/js/main.34ae31d2.chunk.js"
+  },
+  {
+    "revision": "8bbc7a7f32d57d598b05",
+    "url": "/IndecisionApp/static/js/1.8bbc7a7f.chunk.js"
+  },
+  {
+    "revision": "34ae31d2d5f97a853eed",
+    "url": "/IndecisionApp/static/css/main.c8b90117.chunk.css"
+  },
+  {
+    "revision": "8bbc7a7f32d57d598b05",
+    "url": "/IndecisionApp/static/css/1.2f02b3b3.chunk.css"
+  },
+  {
+    "revision": "58c089d5f7143ec68bf7e807ff7a12ac",
+    "url": "/IndecisionApp/index.html"
+  }
+];
